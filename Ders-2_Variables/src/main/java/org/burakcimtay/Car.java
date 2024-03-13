6396d163bf5279b8ce182e4f60085e7d4bca35a5
Burak Çimtay <burakcimtay@gmail.com>
2024-03-14 02:15:52 +0300

    Variables

---
+++ Ders-2_Variables/src/main/java/org/burakcimtay/Car.java
@@ -0,0 +1,23 @@
+package org.burakcimtay;
+
+public class Car {
+
+    public static void main(String[] args) {
+
+        String marka = "Mercedes";
+        int kapi = 5;
+        boolean trPlaka = true;
+        int motorGucu = 160;
+        String motorTuru = "Benzin";
+        String modelYili = "2015";
+        String rengi = "Beyaz";
+
+        System.out.println("Marka: " + marka);
+        System.out.println("Kapı: " + kapi);
+        System.out.println("Türkiye Plaka: " + trPlaka);
+        System.out.println("Motor Türü: " + motorTuru);
+        System.out.println("Motor Gücü: " + motorGucu);
+        System.out.println("Model Yılı: " + modelYili);
+        System.out.println("Renk: " + rengi);
+    }
+}
