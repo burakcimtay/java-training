72107b54a4a1323afab3bc0f9094a31661c250e1
Burak Çimtay <burakcimtay@gmail.com>
2024-03-22 21:39:16 +0300

    Update Method_2.java

--- Ders-6_Methods/src/main/java/org/burakcimtay/Method_2.java
+++ Ders-6_Methods/src/main/java/org/burakcimtay/Method_2.java
@@ -1,11 +1,8 @@
 package org.burakcimtay;
 
 public class Method_2 {
-
-    private static float KupAl(int x){
-        x=x*x*x;
-
-        return x;
+    private static float KupAl(float myNumber){
+        return (float) Math.pow(myNumber, 3);
     }
 
     public static void main(String[] args) {
